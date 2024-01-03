57f712265972752b0edfdefcb036145871dced22
mktech <108585153+mktech07@users.noreply.github.com>
2024-01-03 16:59:16 +0530

    App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = ' https://mktech07.github.io/Profile_Management_-Admin_pannel-/';
+    public string $baseURL = 'https://mktech07.github.io/Profile_Management_-Admin_pannel-/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
